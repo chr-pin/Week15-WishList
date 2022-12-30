95dbb9e82bee254a807876c4c70b227b234566f3
Chr P <christian.pintson@tktk.ee>
2022-12-30 13:18:46 +0200

    Wishlist

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require ('express');
+const Wish = require('./model/wish');
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
+    Wish.fetchAllwishes(WishesFromFile => {
+        console.log(WishesFromFile);
+        res.render('index', {myWishes: WishesFromFile});
+    })
+    
+})
+
+app.post('/wish', (req, res) => {
+
+    let userData = req.body.userWish;
+
+    let newWish = new Wish (userData);
+    newWish.saveWish();
+    res.redirect('/');
+
+})
+
+
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running  ${port}.`);
+})
